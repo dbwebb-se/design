2c8763117518e8e45235f579bec3bb28d848ecc1
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-11-12 17:36:15 +0100

    Update migrate

--- example/migrate/redovisa_01.bash
+++ example/migrate/redovisa_01.bash
@@ -19,23 +19,42 @@ function die
 #
 function main
 {
-    local from="example/redovisa/"
-    local to="me/redovisa/"
+    local from="example/redovisa"
+    local to="me/redovisa"
 
     echo "Migrate '$from' to '$to'."
 
-    [ -d $from ] || die "Missing directory '$to'"
-    [ -d $to ] || die "Missing directory '$to'"
+    [ -d "$from" ] || die "Missing directory '$to'"
+    [ -d "$to" ] || die "Missing directory '$to'"
 
     # content/verktyg
-    rsync -av $from/content/verktyg $to/content/
-    rm -f $to/content/verktyg/999_att-gora.md
+    rsync -av "$from/content/verktyg" "$to/content/"
+    rm -f "$to/content/verktyg/999_att-gora.md"
 
     # view files were updated with codestyle, ignoring them
     #rsync -av $from/view $to/
 
     # theme/Makefile && theme/src && theme/htdocs && htdocs/webfonts
-    
+    local from="example/theme"
+    local to="me/redovisa/theme"
+
+    echo "Migrate '$from' to '$to'."
+
+    [ -d "$from" ] || die "Missing directory '$to'"
+    [ -d "$to" ] || die "Missing directory '$to'"
+
+    # theme/
+    rsync -av "$from/" "$to/"
+
+    # delete stray files in some parts.
+    local parts="src/@desinax/typographic-grid"
+    rsync -av --delete "$from/$parts/" "$to/$parts/"
+
+    parts="src/@fortawesome"
+    rsync -av --delete "$from/$parts/" "$to/$parts/"
+
+    parts="src/normalize.css"
+    rsync -av --delete "$from/$parts/" "$to/$parts/"
 }
 
 main
